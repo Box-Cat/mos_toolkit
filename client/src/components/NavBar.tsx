c8f1d541b7086c3217fc745d597fb1577ec3bbe3
github Box-Cat <github pjh211pjh@gmail.com>
2022-12-03 23:01:37 +0900

    tailwind제거 + nav완성

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,57 +1,62 @@
-import React, { FormEvent, useEffect, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import "./NavBar.css";
+
 //import { FaSearch } from "react-icons/fa";
 //import { useAuthDispatch, useAuthState } from "../context/auth"
 
 const NavBar = () => {
-    //const { loading, authenticated } = useAuthState();
-    //const dispatch = useAuthDispatch();
-    const [errors, setErrors] = useState<any>({});
+  //const { loading, authenticated } = useAuthState();
+  //const dispatch = useAuthDispatch();
+  const [errors, setErrors] = useState<any>({});
 
-    // const handleLogout = () => {
-    //     axios.post("/auth/logout")
-    //         .then(() => {
-    //             dispatch("LOGOUT");
-    //             window.location.reload();
-    //         })
-    //         .catch((error) => {
-    //             console.log(error);
-    //         })
-    // }
+  // const handleLogout = () => {
+  //     axios.post("/auth/logout")
+  //         .then(() => {
+  //             dispatch("LOGOUT");
+  //             window.location.reload();
+  //         })
+  //         .catch((error) => {
+  //             console.log(error);
+  //         })
+  // }
 
-
-    const handleLogout = () => {
-        try {
-            axios.post('/auth/logout').then(() => {
-                toast.success("success!!")
-                window.location.reload();
-            }).catch((error) => {
-                toast.error("fail!!")
-            })
-        }
-        catch (error: any) {
-            console.log('error', error);
-            setErrors(error.response.data || {});
-        }
+  const handleLogout = () => {
+    try {
+      axios
+        .post("/auth/logout")
+        .then(() => {
+          toast.success("success!!");
+          window.location.reload();
+        })
+        .catch((error) => {
+          toast.error("fail!!");
+        });
+    } catch (error: any) {
+      console.log("error", error);
+      setErrors(error.response.data || {});
     }
-    //홈/게임/랭킹/자유게시판/로그아웃 or 로그인,회원가입
-    return (
-        <>
-        <div className="">
-            <span>로그인</span>
-            <span>회원가입</span>
+  };
+  //홈/게임/랭킹/자유게시판/로그아웃 or 로그인,회원가입
+  return (
+    <>
+      <div className="nav">
+        <div className="nav-links nav-auth">
+          <Link to='/auth/login' className="link">로그인</Link>
+          <Link to='/auth/register' className="link">회원가입</Link>
         </div>
-        <div>
-            <span>HOME</span>
-            <span>GAME</span>
-            <span>RANKING</span>
-            <span>FORUM</span>
+        <div className="nav-links">
+          <Link to='/' className="link">HOME</Link>
+          <Link to='/game' className="link">GAME</Link>
+          <Link to='/rank' className="link">RANKING</Link>
+          <Link to='/board' className="link">FORUM</Link>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
-const Home = () => { //1)HTML골격->리액트로 변경 2)CSS->tailwind로 변경  3)JS 기능 순차적 적용 ㄱ.세션나누기 ㄴ.스크롤 ㄷ.스크롤 애니
+const Home = () => {
+  //1)HTML골격->리액트로 변경 2)CSS->tailwind로 변경  3)JS 기능 순차적 적용 ㄱ.세션나누기 ㄴ.스크롤 ㄷ.스크롤 애니
   return (
-    <div >
+   
       <div>
         <div>
           <h1>하데스 모기채</h1>
@@ -14,13 +15,24 @@ const Home = () => { //1)HTML골격->리액트로 변경 2)CSS->tailwind로 변�
             <p>호두도 무자비하게</p>
           </div>
           <div>
-            <p>헤치울 수 있는<br/>최고급 퇴치 기구</p>
+            <p>
+              헤치울 수 있는
+              <br />
+              최고급 퇴치 기구
+            </p>
           </div>
           <div>
-            <p>늦가을 밤<br/>모기 같은 그대에게</p>            
+            <p>
+              늦가을 밤<br />
+              모기 같은 그대에게
+            </p>
           </div>
           <div>
-            <p>심판을 내리는<br/>최고의 도구</p>
+            <p>
+              심판을 내리는
+              <br />
+              최고의 도구
+            </p>
           </div>
         </div>
         <div>SECTION2</div>
@@ -29,8 +41,8 @@ const Home = () => { //1)HTML골격->리액트로 변경 2)CSS->tailwind로 변�
           <div>SECTION4</div>
         </div>
       </div>
-    </div>
-  )
-}
+   
+  );
+};
 
-export default Home
+export default Home;
